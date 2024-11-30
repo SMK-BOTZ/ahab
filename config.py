b115c566df5e71d970040b422aadcd31cf9d209f
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-12-01 03:48:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
+API_ID = int(getenv("API_ID", "28243586"))
+API_HASH = getenv("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
+BOT_TOKEN = getenv("BOT_TOKEN", "7714254527:AAFU4rOeoaab_T4ytlf0G_TQMXZFOTDcekI")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002196541223"))
